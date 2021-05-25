b3a9ba1dfca1011bf9ff0e2b78106f73c39a3e93
bennKaoutar <49061347+bennKaoutar@users.noreply.github.com>
2021-05-25 21:56:42 +0100

    Update SocieteController.php

--- src/Controller/SocieteController.php
+++ src/Controller/SocieteController.php
@@ -12,9 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/societe")
- */
+
 class SocieteController extends AbstractController
 {
     /**
